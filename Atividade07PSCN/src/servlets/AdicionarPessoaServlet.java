dad8d14e580b87cf6735e5a9939298395a40080f
Saulo <saulomcm@gmail.com>
2017-11-16 23:39:42 -0300

    Adiciona o Servlet adicionarPessoa

---
+++ Atividade07PSCN/src/servlets/AdicionarPessoaServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdicionarPessoaServlet
+ */
+@WebServlet("/adicionarPessoa")
+public class AdicionarPessoaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdicionarPessoaServlet() {
+        super();
+    }
+    
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String sobrenome = request.getParameter("sobrenome");
+		PrintWriter out = response.getWriter();
+		out.println(nome+" "+sobrenome);
+	}
+
+}
